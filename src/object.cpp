c2b9cb85ef546ca3679c6bc8a29089e0dc4e7a83
sparth292 <sparthsalunke@gmail.com>
2025-07-15 13:01:22 +0530

    object.cpp changed

--- src/object.cpp
+++ src/object.cpp
@@ -9,6 +9,14 @@
 
 std::string write_blob(std::string& filePath , std::string& objectDir){
 
-    
+    std::ifstream file(filePath , std::ios::binary);
+    if(!file){
+        throw std::runtime_error("File Cannot be opened" + filePath);
+    }
+
+    std::ostringstream buffer;
+    buffer << file.rdbuf();
+    std::string content = buffer.str();
+    file.close();
 
 }
